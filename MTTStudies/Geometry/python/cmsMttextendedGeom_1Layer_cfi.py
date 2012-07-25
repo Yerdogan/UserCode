36dce9cc14f7edd451b934972e4b71c47e761f4b
pmaanen <>
2012-07-25 14:38:35 +0000

    fixed an error

--- MTTStudies/Geometry/python/cmsMttextendedGeom_1Layer_cfi.py
+++ MTTStudies/Geometry/python/cmsMttextendedGeom_1Layer_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for simulation
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
-XMLIdealGeometryESSource.geomXMLfiles.append('MTTStudies/Geometry/data/mttalgo_1Layer.xml')
-XMLIdealGeometryESSource.geomXMLfiles.append('MTTStudies/Geometry/data/mttsens_1Layer.xml')
-XMLIdealGeometryESSource.geomXMLfiles.append('MTTStudies/Geometry/data/mttNumbering.xml')
+from Configuration.StandardSequences.GeometryIdeal_cff import *
+XMLIdealGeometryESSource.geomXMLFiles.append('MTTStudies/Geometry/data/mttalgo_1Layer.xml')
+XMLIdealGeometryESSource.geomXMLFiles.append('MTTStudies/Geometry/data/mttsens_1Layer.xml')
+XMLIdealGeometryESSource.geomXMLFiles.append('MTTStudies/Geometry/data/mttNumbering.xml')
