bcb143c545136f02d963b95a83d8cd95f7cdf755
pmaanen <>
2012-08-08 13:45:02 +0000

    fixed a typo

--- MTTStudies/Geometry/python/cmsMttextendedGeom_cfi.py
+++ MTTStudies/Geometry/python/cmsMttextendedGeom_cfi.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 # Ideal geometry, needed for simulation
 from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
-XMLIdealGeometryESSource.geomXMLfiles.append('MTTStudies/Geometry/data/mttalgo.xml')
-XMLIdealGeometryESSource.geomXMLfiles.append('MTTStudies/Geometry/data/mttsens.xml')
-XMLIdealGeometryESSource.geomXMLfiles.append('MTTStudies/Geometry/data/mttNumbering.xml')
+XMLIdealGeometryESSource.geomXMLFiles.append('MTTStudies/Geometry/data/mttalgo.xml')
+XMLIdealGeometryESSource.geomXMLFiles.append('MTTStudies/Geometry/data/mttsens.xml')
+XMLIdealGeometryESSource.geomXMLFiles.append('MTTStudies/Geometry/data/mttNumbering.xml')
