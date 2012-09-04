c08c91a0722e9901080e0f2f3091dfd9bc66e487
pmaanen <>
2012-09-04 13:07:09 +0000

    missing files.....

---
+++ MTTStudies/MTTDetId/src/classes.h
@@ -0,0 +1,4 @@
+#include "MTTStudies/MTTDetId/interface/MTTPanelId.h"
+#include "MTTStudies/MTTDetId/interface/MTTLayerId.h"
+#include "MTTStudies/MTTDetId/interface/MTTStripId.h"
+#include "MTTStudies/MTTDetId/interface/MTTTileId.h"
